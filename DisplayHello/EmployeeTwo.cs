608695a9ff6bb718372d8e6891283b962d0f6dd4
Yassine ENNAJEM <yassineennajem68@gmail.com>
2020-04-21 23:03:49 +0200

    Add New List Of Employees & Chnage Message Exception Equal Method & Add ToString Method

--- DisplayHello/EmployeeTwo.cs
+++ DisplayHello/EmployeeTwo.cs
@@ -13,7 +13,12 @@ namespace DisplayHello
             {
                 return this.Name == emp.Name && this.DateOfBirth == emp.DateOfBirth;
             }
-            throw new Exception("employee Paramater can be Employee Type !!");
+            throw new Exception("Employee Paramater be Employee 'Type' !!");
+        }
+
+        public override string ToString()
+        {
+            return $"ID = {ID} , Name {Name} , Date Of Birth = {DateOfBirth.Value.ToString("dd/MM/yyyy")}";
         }
     }
 }
--- DisplayHello/Program.cs
+++ DisplayHello/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DisplayHello
 {
@@ -6,7 +7,18 @@ namespace DisplayHello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee { ID = 1, Name = "Jhon", DateOfBirth = new DateTime(1992, 05, 25) },
+                new Employee { ID = 2, Name = "Tod", DateOfBirth = new DateTime(1980, 07, 20) },
+                new Employee { ID = 3, Name = "Sara", DateOfBirth = new DateTime(1999, 01, 15) },
+                new Employee { ID = 4, Name = "ENNAJEM", DateOfBirth = new DateTime(1995, 12, 07) }
+            };
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine(emp.ToString());
+            }
         }
     }
 }
